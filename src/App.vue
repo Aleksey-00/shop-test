b24e014f436b0733d54a8102681f39cb855b3f3e
Alexey Azorabekov <a.azorabekov@presentsimple.com>
2025-02-24 11:57:40 +0300

    Initialize new shop-test project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// Ваш код TypeScript здесь
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1>Привет, Vue 3 + TypeScript + Vuetify!</h1>
+        <v-btn color="primary">Кнопка Vuetify</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
